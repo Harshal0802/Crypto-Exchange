86bb9fea351327b21585c4c1f8d5e93bc2cc4cc4
harshal <harshrathod1111@gmail.com>
2023-08-29 16:49:28 +0530

    completed home screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Header from "./component/Header";
-import Home from "./component/Home";
-import Coin from "./component/Coin";
-import CoinDetails from "./component/CoinDetails";
-import Exchanges from "./component/Exchanges";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './component/Header';
+import Home from './component/Home';
+import Coin from './component/Coin';
+import CoinDetails from './component/CoinDetails';
+import Exchanges from './component/Exchanges';
+import Footer from './component/Footer';
 
 function App() {
   return (
-   <Router>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/coin" element={<Coin />} />
-      <Route path="/exchanges" element={<Exchanges />} />
-      <Route path="/coin/:id" element={<CoinDetails />} />
-    </Routes>
-   </Router>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coin" element={<Coin />} />
+        <Route path="/exchanges" element={<Exchanges />} />
+        <Route path="/coin/:id" element={<CoinDetails />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
